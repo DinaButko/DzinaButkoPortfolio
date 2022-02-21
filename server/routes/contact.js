59defd0b5f7e57b474a67f2e77208ae1240fe7f6
DzinaButko <butkodina@gmail.com>
2022-02-21 10:15:17 -0500

    Add create/read/update options

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -20,10 +20,105 @@ router.get('/', (req,res,next) => {
         {
             //console.log(ContactList);
            
-          res.render('contactlist', {title: 'Contact List', ContactList: contactList}) 
+          res.render('contact/list', {title: 'Contact List', ContactList: contactList}) 
 
         }
     });
 });
+
+/* GET route for the displaying Athe dd Contacts list page - CREATE operation */
+router.get('/add', (req, res, next) => {
+    res.render('contact/add', {title: 'Add Contact'}) 
+
+});
+
+
+/* POST route for the processing the  Add Contacts list page - CREATE operation */
+
+router.post('/add', (req, res, next) => {
+    let newContact = Contact({
+        "name": req.body.name,
+        "phone": req.body.number,
+        "email": req.body.email
+
+    });
+Contact.create(newContact, (err, Contact) => {
+    if(err)
+    {
+        console.log(err);
+        res.end(err);
+    }
+    else{
+        //refresh the contact list
+        res.redirect('/contact-list');
+    }
+})
+});
+
+
+/* GET route for the displaying the  Edit Contacts list page - UPDATE operation */
+router.get('/edit/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    Contact.findById(id, (err, contactToEdit) => {
+        if (err)
+        {
+                console.log(err);
+                res.end(err);
+        }
+        else 
+        {
+                    //show the edit view
+             res.render('contact/edit', {title: 'Edit Contact', contact: contactToEdit})
+        }
+    });
+
+});
+
+
+/* POST route for the processing the  Edit Contacts list page - UPDATE operation */
+router.post('/add', (req, res, next) => {
+ let id = req.params.id
+
+ let updateContact = Contact({
+     "_id": id, 
+    "name": req.body.name,
+    "phone": req.body.number,
+    "email": req.body.email
+ });
+
+ Contact.updateOne({_id: id}, updateCpntact, (err) => {
+     if(err)
+     {
+         console.log(err);
+         res.end(err);
+     }
+     else 
+     {
+          //refresh the contact list
+        res.redirect('/contact-list');
+     }
+
+ });
+});   
+
+/* GET to perform contacts deletion - DELETE operation */
+router.get('/delete/:id', (req, res, next) => {
+    let id = req.params.id;
+    Contact.remove({_id: id}, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else 
+        {
+             //refresh the contact list
+           res.redirect('/contact-list');
+        }
+    });
+
+});
+
 
 module.exports = router;
