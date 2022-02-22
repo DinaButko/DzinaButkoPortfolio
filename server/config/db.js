a69ab349afe3623006ecdb9930d11d75bdbe0a6a
DzinaButko <butkodina@gmail.com>
2022-02-22 15:24:58 -0500

    updated vies

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports =
 {
-"URI" : "mongodb://127.0.0.1/contact_list"
-
+//"URI" : "mongodb://127.0.0.1/contact_list"
+"URI" : "mongodb+srv://dina:aTuePca3jJGkra2q@mongodbserver.bkkxb.mongodb.net/contact_list?retryWrites=true&w=majority"
 }
